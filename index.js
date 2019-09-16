1b4ccb1e8625a411f7cee4e0af46af65e2d44a4a
Erika Miwa <erikamiwa612@gmail.com>
2019-09-16 00:15:58 -0700

    add cors on index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const cors = require('cors');
+app.use(cors());
 
 const ATLAS_URI = process.env.ATLAS_URI;
 
